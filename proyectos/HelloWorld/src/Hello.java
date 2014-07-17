75bb5d6d230e484f73d66897a7eb5c8b0bfb978c
Wilfredo Villanueva <wilfredo.nge@gmail.com>
2014-07-16 20:56:30 -0500

    subiendo proyecto
    subida

---
+++ proyectos/HelloWorld/src/Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+	private int a;
+	private int b;
+	
+	public Hello(){
+		this.a = 10;
+		this.b = 20;
+	}
+	
+	public int suma(int a, int b){
+		return a + b;
+	}
+	
+	public int resta(int a, int b){
+		return a - b;
+	}
+}
